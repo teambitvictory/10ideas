3806578ece4bb89d658bc402726c3a4213f66747
Stefan Blamberg <stefan.blamberg@sap.com>
2021-07-07 22:59:05 +0200

    Apply custom theme to navigation

--- src/components/main/TabNavigation.jsx
+++ src/components/main/TabNavigation.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useTheme } from 'react-native-elements';
 import BookmarksScreen from '../bookmarks/BookmarksScreen';
 import DashboardScreen from '../dashboard/DashboardScreen';
 import FeedScreen from '../feed/FeedScreen';
@@ -25,27 +26,38 @@ const screens = [
   },
 ];
 
-const TabNavigation = () => (
-  <NavigationContainer>
-    <TabNavigator.Navigator>
-      {screens.map((screen) => (
-        <TabNavigator.Screen
-          key={screen.name}
-          name={screen.name}
-          component={screen.component}
-          options={{
-            tabBarIcon: ({ color, size }) => ( // eslint-disable-line react/prop-types
-              <Icon
-                name={screen.iconName}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-      ))}
-    </TabNavigator.Navigator>
-  </NavigationContainer>
-);
+const TabNavigation = () => {
+  const { theme } = useTheme();
+  const navigationTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: theme.colors.primary,
+    },
+  };
+
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <TabNavigator.Navigator initialRouteName="Home">
+        {screens.map((screen) => (
+          <TabNavigator.Screen
+            key={screen.name}
+            name={screen.name}
+            component={screen.component}
+            options={{
+              tabBarIcon: ({ color, size }) => ( // eslint-disable-line react/prop-types
+                <Icon
+                  name={screen.iconName}
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+        ))}
+      </TabNavigator.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default TabNavigation;
